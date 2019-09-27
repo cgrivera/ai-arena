e32078a7c20f8526a0615ec091fed1af392af56f
staleew1 <edward.staley@jhuapl.edu>
2019-09-27 13:43:37 -0400

    atari update

--- examples/atari/make_env.py
+++ examples/atari/make_env.py
@@ -1,11 +1,18 @@
 import gym
+from stable_baselines.common.atari_wrappers import make_atari, wrap_deepmind
 from arena5.wrappers.single_agent_wrappers import *
+from mpi4py import MPI
 
 def make_env():
-	env = gym.make("PongDeterministic-v4")
-	env = GrayscaleWrapper(env) #also normalizes
-	env = ImageDownsizeWrapper(env, 64)
-	env = FrameStackWrapper(env, 4)
+	# env = gym.make("PongNoFrameskip-v4")
+	# env = GrayscaleWrapper(env) #also normalizes
+	# env = ImageDownsizeWrapper(env, 64)
+	# env = FrameStackWrapper(env, 4)
+
+	env = wrap_deepmind(make_atari("PongNoFrameskip-v4"))
+	workerseed = MPI.COMM_WORLD.Get_rank()*10000
+	env.seed(workerseed)
+
 	env = single_agent_wrapper(env)
 	return env
 	
