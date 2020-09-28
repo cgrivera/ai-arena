676bd4b83382ee4c672136b557a6e29020379b1c
Corban Rivera <corban.rivera@jhuapl.edu>
2020-09-27 23:10:33 -0400

    added policies for image observations in sac

--- arena5/algos/sac/sac_policy.py
+++ arena5/algos/sac/sac_policy.py
@@ -19,9 +19,12 @@ class SACPolicy():
         local_steps = int(num_steps / self.comm.Get_size())
 
         print(self.env.action_space, self.env.observation_space)
-
+        policy = core.MLPActorCritic
+        print('observation space shape', self.env.observation_space.shape)
+        if len(self.env.observation_space.shape)>1:
+            policy = core.ConvActorCritic
         sac(lambda: self.env, self.comm, data_dir, policy_record, self.eval_mode,
-        	actor_critic=core.MLPActorCritic,
+        	actor_critic=policy,
             ac_kwargs=dict(hidden_sizes=[256,256]), 
             gamma=0.99, seed=1337,steps_per_epoch=local_steps, epochs=1,
             replay_size=self.worker_replay_size, logger_kwargs=None)
